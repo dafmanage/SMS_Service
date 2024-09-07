b3015b0e0c9ccdff8c526de4dcd39e635fb1bccf
matos-coder <matiasashenafi0@gmail.com>
2024-09-07 09:59:33 +0300

    security done

--- SMSSERVICE.API/SMSServiceAPI/Program.cs
+++ SMSSERVICE.API/SMSServiceAPI/Program.cs
@@ -102,13 +102,26 @@ builder.Services.AddSwaggerGen();
 
 
 var app = builder.Build();
-//app.Use(async (context, next) =>
-//{
-//    context.Response.Headers.Remove("Server");
-//    context.Response.Headers.Remove("X-Powered-By");
-//    context.Response.Headers.Remove("X-AspNet-Version");
-//    await next();
-//});
+
+//version disclosure
+app.Use(async (context, next) =>
+{
+    context.Response.Headers.Remove("Server");
+    context.Response.Headers.Remove("X-Powered-By");
+    context.Response.Headers.Remove("X-AspNet-Version");
+    await next();
+});
+//missing header
+app.Use(async (context, next) =>
+{
+    context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
+    context.Response.Headers.Add("X-Frame-Options", "DENY");
+    context.Response.Headers.Add("Strict-Transport-Security", "max-age=31536000; includeSubDomains");
+    context.Response.Headers.Add("Referrer-Policy", "strict-origin-when-cross-origin");
+    context.Response.Headers.Add("Content-Security-Policy", "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline';");
+
+    await next();
+});
 app.UseDeveloperExceptionPage();
 
 // Configure the HTTP request pipeline.
